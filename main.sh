b8cbead6f11f7f9d4ad4f3d22380a2bf2cf45c6a
Imran Remtulla <iremtulla15@gmail.com>
2020-08-11 03:41:32 -0400

    Put main in loop

--- main.sh
+++ main.sh
@@ -9,48 +9,51 @@ serverFindConnection
 echo 'Connection found.'
 echo ''
 resticAskPass
-echo ''
-echo "Pick an action: "
-echo '1) Backup'
-echo '2) Forget'
-echo '3) Prune'
-echo '4) List Backup Snapshots'
-echo '5) Check Backup Integrity'
-echo '6) Freeform'
-echo '*) Exit'
 
-read choice	
-case $choice in	
-	1)
-		DATE="$(date +"%Y-%m-%d-%H-%M")"
-    	resticCommand "init"  2>/dev/null
-    	resticCommand "backup $LOCALPATH -v --exclude \".stversions\" > ~/restic-backup-$DATE.log" &
-		echo "Command is being run asynchronously on the Server, with output saved in the home directory. Attempt to print output? Y/N"
-		read printOutput
-		if [ $printOutput == 'y' ] || [ $printOutput == 'Y' ]; then sleep 3; serverCommand "tail -f ~/restic-backup-$DATE.log"; fi
-		;;	
-	2)
-		resticCommand "forget ${RETENTIONPOLICY}"
-		;;
-	3)
-		DATE="$(date +"%Y-%m-%d-%H-%M")"
-		resticCommand "prune > ~/restic-prune-$DATE.log" &
-		echo "Command is being run asynchronously on the Server, with output saved in the home directory. Attempt to print output? Y/N"
-		read printOutput
-		if [ $printOutput == 'y' ] || [ $printOutput == 'Y' ]; then sleep 3; serverCommand "tail -f ~/restic-prune-$DATE.log"; fi
-		;;	
-	4)
-		resticCommand "snapshots"
-		;;		
-	5)
-		resticCommand "check"
-		;;	
-	6)
-		echo 'Enter the Restic command: '
-		read RC
-		resticCommand "${RC}"
-		;;		
-	*)
-		exit
-		;;
-esac
+while [ true ]; do
+	echo ''
+	echo "Pick an action: "
+	echo '1) Backup'
+	echo '2) Forget'
+	echo '3) Prune'
+	echo '4) List Backup Snapshots'
+	echo '5) Check Backup Integrity'
+	echo '6) Freeform'
+	echo '*) Exit'	
+
+	read choice	
+	case $choice in	
+		1)
+			DATE="$(date +"%Y-%m-%d-%H-%M")"
+	    	resticCommand "init"  2>/dev/null
+	    	resticCommand "backup $LOCALPATH -v --exclude \".stversions\" > ~/restic-backup-$DATE.log" &
+			echo "Command is being run asynchronously on the Server, with output saved in the home directory. Attempt to print output? Y/N"
+			read printOutput
+			if [ $printOutput == 'y' ] || [ $printOutput == 'Y' ]; then sleep 3; serverCommand "tail -f ~/restic-backup-$DATE.log"; fi
+			;;	
+		2)
+			resticCommand "forget ${RETENTIONPOLICY}"
+			;;
+		3)
+			DATE="$(date +"%Y-%m-%d-%H-%M")"
+			resticCommand "prune > ~/restic-prune-$DATE.log" &
+			echo "Command is being run asynchronously on the Server, with output saved in the home directory. Attempt to print output? Y/N"
+			read printOutput
+			if [ $printOutput == 'y' ] || [ $printOutput == 'Y' ]; then sleep 3; serverCommand "tail -f ~/restic-prune-$DATE.log"; fi
+			;;	
+		4)
+			resticCommand "snapshots"
+			;;		
+		5)
+			resticCommand "check"
+			;;	
+		6)
+			echo 'Enter the Restic command: '
+			read RC
+			resticCommand "${RC}"
+			;;		
+		*)
+			exit
+			;;
+	esac
+done
